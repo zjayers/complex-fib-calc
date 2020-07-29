713a1ae8c015437e1cf9063e99a14161a7d8a54d
Zach Ayers <ayers.dev@icloud.com>
2020-07-29 17:30:59 -0500

    init

--- worker/index.js
+++ worker/index.js
@@ -1,10 +1,10 @@
-const keys = require("./keys");
-const redis = require("redis");
+const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000,
+  retry_strategy: () => 1000
 });
 const sub = redisClient.duplicate();
 
@@ -13,7 +13,7 @@ function fib(index) {
   return fib(index - 1) + fib(index - 2);
 }
 
-sub.on("message", (channel, message) => {
-  redisClient.hset("values", message, fib(parseInt(message)));
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
 });
-sub.subscribe("insert");
+sub.subscribe('insert');
